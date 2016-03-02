9035c494ab673a45247c6610f1ce009d6311f67c
Artyom Pertsovsky <artyom@pertsovsky.com>
2016-03-03 00:37:04 +0200

    Bump up version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExIntercom.Mixfile do
 
   def project do
     [app: :exintercom,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.2",
      description: description,
      package: package,
